fad6f202e25e67e44c60eedfd2137fb7bb7a898c
Faridhfatush <faridhfatush2024@gmail.com>
2022-06-28 23:57:19 +0300

    added code

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,46 @@
-var diffEls = document.querySelectorAll(".diff__btn");
-var diffEl = document.querySelector(".diff__btn.active").innerHTML;
-var n = diffEl;
-var colorsEl = document.querySelector(".colors");
-var colorsBlocks;
-var rgbEl = document.querySelector(".rgb");
-var statusEl = document.querySelector(".status");
-var colors = [];
-createBlocks(n);
-resetGame();
+let diffEls = document.querySelectorAll(".diff__btn");
+let six = document.querySelector(".six");
+let nine = document.querySelector(".nine");
+let colorsEl = document.querySelector(".colors");
+let colorsBlocks;
+let rgbEl = document.querySelector(".rgb");
+let h1_bg_change = document.getElementById('change');
+let statusEl = document.querySelector(".status");
+let n = 6;
+six.classList.add('active');
+six.addEventListener('click', () => {
+  six.classList.add('active');
+  n = six.innerHTML;
+  if (nine.classList.contains('active')) {
+    nine.classList.remove('active');
+  }
+  createBlocks(n);
+  resetGame();
+})
 
-function checkColors(e) {
-  // your code here
+nine.addEventListener('click', () => {
+  n = nine.innerHTML;
+  nine.classList.add('active');
+  if (six.classList.contains('active')) {
+    six.classList.remove('active');
+  }
+  createBlocks(n);
+  resetGame()
+})
+
+
+
+
+
+let colors = [];
+createBlocks(n);
+window.onload = resetGame();
+
+function displayColor() {
+ let pickedColor = random(n);
+  let randClr = colors[pickedColor];
+  return randClr;
+
 }
 
 function resetGame() {
@@ -18,21 +48,22 @@ function resetGame() {
   document.body.style.color = "black";
   colors = [];
   pickColors();
-  pickedColor = random(n);
-  rgbEl.innerHTML = colors[pickedColor];
+  rgbEl.innerHTML = displayColor();
   setColors();
   statusEl.innerHTML =
-    "Try to guess the right color based on the RGB value by clicking on the blocks.";
+    `Try to guess the right color based on the RGB value by clicking on the blocks.`;
+  statusEl.style.color = '';
+  h1_bg_change.style.backgroundColor = '';
 }
 
 function setColors() {
-  for (var i = 0; i < colorsBlocks.length; i++) {
+  for (let i = 0; i < colorsBlocks.length; i++) {
     colorsBlocks[i].style.backgroundColor = colors[i];
   }
 }
 
 function pickColors() {
-  for (var i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     colors.push(randomColor());
   }
 }
@@ -45,21 +76,38 @@ function random(r) {
   return Math.floor(Math.random() * r);
 }
 
-function setNumberOfTiles(e) {
-  // your code here
+function checkColors(blockClr, block, colorsBlocks) {
+
+  let correctColor = rgbEl.textContent;
+  if (blockClr === correctColor) {
+    h1_bg_change.style.backgroundColor = correctColor;
+    colorsBlocks.forEach(block => {
+      block.style.backgroundColor = correctColor;
+    })
+    statusEl.textContent = `Finally! Correct Guess!`;
+    statusEl.style.color = `${correctColor}`;
+
+  } else {
+    block.style.backgroundColor = block.style.backgroundColor;
+    statusEl.textContent = `Keep guessing...!`;
+    statusEl.style.color = 'black';
+  }
 }
 
 function createBlocks(num) {
   colorsEl.innerHTML = "";
 
   // here is an example of a loop that is used to create the blocks of color depending on you choice ie 6 or 9, however you need to add event listeners
-  for (var i = 0; i < num; i++) {
-    var block = document.createElement("div");
+  for (let i = 0; i < num; i++) {
+    let block = document.createElement("div");
     block.classList.add("colors__block");
     colorsEl.appendChild(block);
   }
   colorsBlocks = document.querySelectorAll(".colors__block");
-  for (var i = 0; i < colorsBlocks.length; i++) {
-    colorsBlocks[i].addEventListener("click", checkColors);
-  }
+  
+  colorsBlocks.forEach(block => {
+    block.addEventListener('click', () => {
+      checkColors(block.style.backgroundColor, block, colorsBlocks);
+    })
+  })
 }
